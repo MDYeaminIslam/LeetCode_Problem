34664476277e7d34bbe714978fb805e0e66cbce6
Md. Yeamin Islam <yeamin15-13529@diu.edu.bd>
2023-12-28 11:53:47 +0600

    valid parenthesis problem solve

---
+++ 20.Valid_parenthesis.js
@@ -0,0 +1,20 @@
+const prompt = require('prompt-sync')();
+
+function isValid(s) {
+  let stack = [];
+  let match = {
+    ')': '(',
+    '}': '{',
+    ']': '['
+  };
+  for (const char of s){
+    if(match[char]){
+      if(!stack || stack.pop() !== match[char]) return false;
+    }
+    else stack.push(char);
+  }
+  return stack.length === 0;
+}
+
+s = '{[()]}'
+console.log(isValid(s)) // true
